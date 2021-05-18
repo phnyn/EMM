b15ddf1e1ab2314e256bcf7fd736fc485fd305ae
Phuong Thi Nguyen <nguyen_thi_phuong@live.de>
2021-05-18 02:32:15 +0200

    Added movements

--- Abgabe1/Assets/Script/ButtonEventHandler.cs
+++ Abgabe1/Assets/Script/ButtonEventHandler.cs
@@ -1,47 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
-using UnityEngine.Events;
 
 public class ButtonEventHandler : MonoBehaviour
 {
-    //public Transform transform;
-    public GameObject vbs;
-    float _speed = 1f;
-    // Start is called before the first frame update
-    void Start()
-    {   vbs = GameObject.Find("btn1");
-        VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
-        for(int i=0; i < vbs.Length; ++i){
+    public Transform player;
+    public float _speed = 3f;
+    void Start() 
+    {        
+        VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour> ();
+        for (int i = 0; i < vbs.Length; ++i) {
+        // Register with the virtual buttons TrackableBehaviour
             vbs[i].GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
             vbs[i].GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
+            Debug.Log("Hier: "+ vbs[i].GetComponent<VirtualButtonBehaviour>().VirtualButtonName);
         }
     }
 
     void OnButtonPressed(VirtualButtonBehaviour vb){
+        String button = vb.VirtualButtonName;
+
+        //rechts
+        if(button == "btn1"){ 
+            player.transform.rotation *= Quaternion.Euler(20,0,0); 
+            Debug.Log("right");
+        }
+
+        if(button == "btn2"){
+            player.transform.position += new Vector3(0,0,0.5f)*_speed;
+            Debug.Log("up");
+
+        }
+
+        if(button == "btn3"){
+            player.transform.position += new Vector3(0,0,-0.5f)*_speed;
+            Debug.Log("down");
+
+        }
+        //links
+        if(button == "btn4"){
+            player.transform.Rotate(Vector3.left*_speed); 
+            Debug.Log("left");
+        }
+
+        /*
         switch(vb.VirtualButtonName){
             case "btn1": //rechts
-                    transform.localPosition = Vector3.forward * Time.deltaTime *_speed;
-                    Debug.Log("rechts");
+                    Debug.Log("right");
                 break;
             case "btn2": //oben
-                                transform.Translate(0,0,1);
-                                Debug.Log("oben");
-
-                    // transform.position += Vector3.forward * Time.deltaTime * _speed;
+                    player.transform.position += new Vector3(0,0,0.1f)*_speed;
+                    Debug.Log("up");
                 break;
             case "btn3": //unten
-                            transform.Translate(0,0,1);
-                            Debug.Log("Unten");
-
-                    //transform.position += Vector3.forward * Time.deltaTime * _speed;
+                    Debug.Log("down");
                 break;
             case "btn4": //links
-                                transform.Translate(0,0,1);
-                                Debug.Log("Links");
-
-                     //transform.position += Vector3.forward * Time.deltaTime * _speed;
+                    Debug.Log("left");
                 break;
-        }
+        }  */
     }
 
     void OnButtonReleased(VirtualButtonBehaviour vb){
